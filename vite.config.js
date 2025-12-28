4711fdfe3323f4e7381e7782ea8768e1afd1897f
Dave Anaya <dave@catincloudlabs.com>
2025-12-28 17:34:13 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      // This tells Vite: "Forward any request starting with /data to S3"
+      '/data': {
+        target: 'https://catincloud-io-public.s3.us-east-1.amazonaws.com/data',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/data/, '') 
+      }
+    }
   }
 })
